027287d3d123a7286acad269c62f219f25f51c6b
Илья Александров <kein1999@outlook.com>
2018-10-02 00:44:05 +0300

    новая вьюшка base

--- test_django/fomiss/lesson_third/urls.py
+++ test_django/fomiss/lesson_third/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     re_path(r'^filters/$', views.filter),
     re_path(r'^tags-if/$', views.tags_if),
     re_path(r'^tags-for/$', views.tags_for),
+    re_path(r'^base/$', views.base),
     ]
